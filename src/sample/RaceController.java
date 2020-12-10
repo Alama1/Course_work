18bc7317813fdb5be63849a6d90e34d836b4ac51
Alama1 <oldpaladin7@gmail.com>
2020-12-10 22:12:03 +0200

    2.0

--- src/sample/RaceController.java
+++ src/sample/RaceController.java
@@ -3,11 +3,17 @@ package sample;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class RaceController {
+    private List racesDest;
     String carrier;
     String destination;
     String departureDate;
@@ -23,26 +29,20 @@ public class RaceController {
     private URL location;
 
     @FXML
-    private Label greeter;
-
-    @FXML
-    private Label race_list;
-
+    private Label races;
     @FXML
     void initialize() throws SQLException, ClassNotFoundException {
+        racesDest = new ArrayList();
         DatabaseHandler dbHandler = new DatabaseHandler();
         ResultSet result = dbHandler.getRace();
         int counter = 0;
         while (result.next()) {
-            carrier = (result.getString(2));
             destination = (result.getString(3));
-            departureDate = (result.getString(4));
-            flight_time = (result.getString(5));
-            transplant = (result.getString(6));
-            price = (result.getString(7));
-            seats_left = (result.getString(8));
-            counter++;
+            racesDest.add(result.getString(3));
         }
-        race_list.setText("Перевозчик: " + carrier + "\n" + " Направление: " + destination + "\n" +  " Время отправки: " + departureDate + "\n" +  " Время полета: " + flight_time + " Часа "  + "\n" +  " Цена: " + price + "\n" + " Мест осталось: " + seats_left);
+        races.setText(racesDest.get(1).toString());
     }
+
+
+
 }
